87b449699a49d51869d2339761edb382a6bb13ec
Ted Werbel <werbel.ted@gmail.com>
2017-03-16 15:08:04 -0400

    removed bottom border for last switch

--- src/Components/Common/FormSwitch.js
+++ src/Components/Common/FormSwitch.js
@@ -1,53 +1,5 @@
 import React, { Component } from 'react';
-import { View, TouchableOpacity, Text, AlertIOS, TextInput, Switch,  } from 'react-native'
-import FaIcon from 'react-native-vector-icons/FontAwesome'
-
-const styles = {
-  container: {
-    flexDirection: "row",
-    marginLeft: 25,
-    borderBottomWidth: 1,
-    paddingBottom: 10,
-    marginRight: 25,
-    marginTop: 20,
-    borderColor: "#f5f5f5"
-  },
-  basketIcon: {
-    color: "#a6a6a6",
-    paddingLeft: 20,
-    paddingRight: 5,
-    alignSelf: "center",
-    flex: 1
-  },
-  infoContainer: {
-    flex: 8,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center"
-  },
-  arrowIcon: {
-    alignSelf: "center",
-    color: "#8b8b8b",
-    flex: 0.5,
-    marginTop: 12,
-    opacity: 1
-  },
-  label: {
-    fontFamily: "Open Sans",
-    fontSize: 15,
-    letterSpacing: 0.25,
-    color: "#252525",
-    marginBottom: 2,
-    backgroundColor: "transparent",
-  },
-  field: {
-    fontFamily: "Open Sans",
-    fontSize: 16,
-    color: "#252525",
-    marginTop: 3,
-    height: 20
-  },
-}
+import { View, Text, Switch } from 'react-native'
 
 class FormSwitch extends Component {
   constructor(props) {
@@ -71,6 +23,52 @@ class FormSwitch extends Component {
   }
 
   render() {
+    const styles = {
+      container: {
+        flexDirection: "row",
+        marginLeft: 25,
+        borderBottomWidth: this.props.bottom ? 0 : 1,
+        paddingBottom: 10,
+        marginRight: 25,
+        marginTop: 20,
+        borderColor: "#f5f5f5"
+      },
+      basketIcon: {
+        color: "#a6a6a6",
+        paddingLeft: 20,
+        paddingRight: 5,
+        alignSelf: "center",
+        flex: 1
+      },
+      infoContainer: {
+        flex: 8,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center"
+      },
+      arrowIcon: {
+        alignSelf: "center",
+        color: "#8b8b8b",
+        flex: 0.5,
+        marginTop: 12,
+        opacity: 1
+      },
+      label: {
+        fontFamily: "Open Sans",
+        fontSize: 15,
+        letterSpacing: 0.25,
+        color: "#252525",
+        marginBottom: 2,
+        backgroundColor: "transparent",
+      },
+      field: {
+        fontFamily: "Open Sans",
+        fontSize: 16,
+        color: "#252525",
+        marginTop: 3,
+        height: 20
+      },
+    }
     const { placeholder, label, returnKeyType, value, disabled, keyboardType, maxLength, autoCapitalize } = this.props;
 
     return (
