793188c2fefe98b240d71e9d7163b229b47aa959
Ted Werbel <werbel.ted@gmail.com>
2017-03-17 01:24:00 -0400

    updated security screen

--- src/Components/Main/Settings/Security.js
+++ src/Components/Main/Settings/Security.js
@@ -1,10 +1,45 @@
 import React, { Component } from 'react'
-import { Text } from 'react-native'
+import { Text, View, TouchableOpacity, ScrollView } from 'react-native'
+import LinearGradient from 'react-native-linear-gradient'
+import Icon from "react-native-vector-icons/Ionicons"
+import { Actions } from 'react-native-router-flux'
+import { ActionForm, Form, FormField, FormSwitch, FormLabel, FormButton, BackButton, SaveButton } from '../../Common'
+import Header from './Header'
+import * as Animatable from 'react-native-animatable'
 
 export default class Security extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = ({
+      buttonText: "MEDIUM",
+      scrollEnabled: true,
+      disableButtons: false,
+      disableSwitches: false,
+    })
+  }
+
   render() {
+    const { loading, disableButtons, disableSwitches, buttonText } = this.state;
+
     return (
-      <Text>test</Text>
+      <LinearGradient colors={['#fff', '#f5f5f8']} style={{ flex: 1 }}>
+        <ScrollView scrollEnabled={this.state.scrollEnabled}>
+          <View>
+            <BackButton loading={loading} />
+            <Header text="SECURITY" loading={loading} />
+          </View>
+
+          <Form loading={loading} height={340}>
+            <FormButton disabled={disableSwitches} label="Change Password" />
+            <FormButton disabled={disableSwitches} label="Passcode and Touch ID" />
+            <FormButton disabled={disableSwitches} label="Security Questions" bottom />
+            <FormButton disabled={disableSwitches} label="Delete Account" offset={45} centered danger bottom />
+          </Form>
+
+          <SaveButton text={buttonText} width={275} onPress={this.handleToggle} loading={loading} disabled={true} offset={100} disableAnimation/>
+        </ScrollView>
+      </LinearGradient>
     );
   }
 }
