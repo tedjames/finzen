f9f0d7955bdf27ee787823dcd6aca19457ac7dd7
Ted Werbel <werbel.ted@gmail.com>
2017-03-19 20:43:58 -0400

    updated formLabel component

--- src/Components/Common/FormLabel.js
+++ src/Components/Common/FormLabel.js
@@ -1,19 +1,47 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { Text, View } from 'react-native'
 
 const FormLabel = (props) => {
-  const label = props.text.toUpperCase();
-  const labelStyle = {
-    fontFamily: "Montserrat",
-    fontSize: 10,
-    letterSpacing: 1.4,
-    color: "#879099",
-    marginBottom: 2,
-    backgroundColor: "transparent"
+  const { text } = props;
+  const label = text.toUpperCase();
+  const styles = {
+    label: {
+      fontFamily: "Montserrat",
+      fontSize: 10,
+      letterSpacing: 1.4,
+      color: "#879099",
+      alignSelf: "center",
+      backgroundColor: "transparent"
+    },
+    leftDivider: {
+      borderBottomWidth: 1,
+      borderColor: "#f5f5f5",
+      paddingBottom: 10,
+      width: 100,
+      marginRight: 10,
+      marginLeft: 10,
+      position: "relative",
+      bottom: 5
+    },
+    rightDivider: {
+      borderBottomWidth: 1,
+      borderColor: "#f5f5f5",
+      paddingBottom: 10,
+      width: 100,
+      marginRight: 10,
+      marginLeft: 10,
+      position: "relative",
+      bottom: 5
+    }
   }
 
   return (
-    <Text style={labelStyle}>{label}</Text>
+    <View style={{ flexDirection: "row", justifyContent: "space-around" }}>
+      <View style={styles.leftDivider} />
+      <Text style={styles.label}>{label}</Text>
+      <View style={styles.rightDivider} />
+    </View>
+
   );
 };
 
