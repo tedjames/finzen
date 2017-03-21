d721b149df36da0694f376895831648c8f5d27cb
Ted Werbel <werbel.ted@gmail.com>
2017-03-21 11:37:27 -0400

    cleaned up code and added eslint inline exceptions

--- src/Components/Common/GradientView.js
+++ src/Components/Common/GradientView.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// Fixes eslint false positive for an import error in this file
 /* eslint-disable import/no-unresolved */
 /* eslint-disable import/extensions */
 import LinearGradient from 'react-native-linear-gradient';
--- src/Components/Common/SaveButton.js
+++ src/Components/Common/SaveButton.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import { TouchableOpacity, View } from 'react-native'
-import Spinner from 'react-native-spinkit'
-import * as Animatable from 'react-native-animatable'
-import LinearGradient from 'react-native-linear-gradient'
-import Snackbar from 'react-native-snackbar'
-import { Actions } from 'react-native-router-flux'
+import { TouchableOpacity, View } from 'react-native';
+import Spinner from 'react-native-spinkit';
+import * as Animatable from 'react-native-animatable';
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/extensions */
+import LinearGradient from 'react-native-linear-gradient';
+/* eslint-enable import/no-unresolved */
+/* eslint-enable import/extensions */
+import Snackbar from 'react-native-snackbar';
+import { Actions } from 'react-native-router-flux';
 
 const styles = {
   saveButton: {
-    width: "100%",
-    height: "100%",
-    alignSelf: "center",
-    alignItems: "center",
-    justifyContent: "center",
+    width: '100%',
+    height: '100%',
+    alignSelf: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
     borderRadius: 10,
     paddingTop: 18,
     paddingBottom: 18,
@@ -24,16 +28,16 @@ const styles = {
     elevation: 2,
   },
   buttonText: {
-    fontFamily: "Montserrat",
+    fontFamily: 'Montserrat',
     fontSize: 12,
     letterSpacing: 1.25,
-    position: "relative",
+    position: 'relative',
     bottom: 0,
     opacity: 1,
-    alignSelf: "center",
-    backgroundColor: "transparent",
-    color: "#fff",
-    fontWeight: "400",
+    alignSelf: 'center',
+    backgroundColor: 'transparent',
+    color: '#fff',
+    fontWeight: '400',
     shadowColor: '#000',
     shadowOffset: { width: 0.5, height: 2 },
     shadowOpacity: 0,
@@ -44,7 +48,7 @@ const styles = {
     opacity: 0,
     marginBottom: 0,
   }
-};;
+};
 
 class SaveButton extends Component {
   constructor(props) {
@@ -57,86 +61,90 @@ class SaveButton extends Component {
   componentWillReceiveProps(nextProps) {
     const { offset, disableAnimation } = this.props;
     if (nextProps.loading && !disableAnimation) {
-      this.refs.spinner.transitionTo({ opacity: 0.75 }, 2000 )
-      this.refs.buttonTextContainer.transitionTo({ bottom: 122 }, 1)
-      this.refs.buttonText.transitionTo({ shadowColor: '#000', shadowOpacity: 0.2, shadowRadius: 4, opacity: 1, transform: [{ scale: 1.05 }] }, 6500)
-      this.refs.saveButton.transitionTo({ width: 400, height: 700, borderRadius: 0, marginTop: 0, bottom: offset ? offset : 150 }, 300, "ease-in-out")
+      this.refs.spinner.transitionTo({ opacity: 0.75 }, 2000);
+      this.refs.buttonTextContainer.transitionTo({ bottom: 122 }, 1);
+      this.refs.buttonText.transitionTo({ shadowColor: '#000', shadowOpacity: 0.2, shadowRadius: 4, opacity: 1, transform: [{ scale: 1.05 }] }, 6500);
+      this.refs.saveButton.transitionTo({ width: 400, height: 700, borderRadius: 0, marginTop: 0, bottom: offset ? offset : 150 }, 300, 'ease-in-out');
     }
     if (nextProps.success) {
-      this.handleSuccess()
+      this.handleSuccess();
     }
 
     if (nextProps.fail) {
-      this.handleFail()
+      this.handleFail();
     }
   }
 
-  renderSpinner() {
-    const { loading, disableAnimation } = this.props;
-    if (loading && !disableAnimation) {
-      return (
-        <Spinner isVisible={true} type="Arc" size={250} color="#3ee8ff" />
-      );
-    } return null;
-  }
-
   handleSuccess() {
-    this.refs.buttonText.transitionTo({ transform: [{ scale: 1.075 }] }, 375, "ease-in")
+    this.refs.buttonText.transitionTo({ transform: [{ scale: 1.075 }] }, 375, 'ease-in');
     setTimeout(() => {
-      this.refs.buttonTextContainer.transitionTo({ left: 200, opacity: 0 }, 200, "ease-in")
-      this.refs.spinner.transitionTo({ opacity: 0.25 }, 200, "ease-in" )
+      this.refs.buttonTextContainer.transitionTo({ left: 200, opacity: 0 }, 200, 'ease-in');
+      this.refs.spinner.transitionTo({ opacity: 0.25 }, 200, 'ease-in');
       setTimeout(() => {
         Actions.pop();
         Snackbar.show({
           title: 'Changes were successfully saved',
           duration: Snackbar.LENGTH_SHORT,
         });
-      }, 200)
-    }, 400)
+      }, 200);
+    }, 400);
   }
 
   handleFail() {
-    this.refs.buttonText.transitionTo({ transform: [{ scale: 1.075 }] }, 375, "ease-in")
+    this.refs.buttonText.transitionTo({ transform: [{ scale: 1.075 }] }, 375, 'ease-in');
     setTimeout(() => {
-      this.refs.buttonTextContainer.transitionTo({ left: 200, opacity: 0 }, 200, "ease-in")
-      this.refs.spinner.transitionTo({ opacity: 0.25 }, 200, "ease-in" )
+      this.refs.buttonTextContainer.transitionTo({ left: 200, opacity: 0 }, 200, 'ease-in');
+      this.refs.spinner.transitionTo({ opacity: 0.25 }, 200, 'ease-in');
       setTimeout(() => {
         Actions.pop();
         Snackbar.show({
           title: 'Changes were unable to be saved',
           duration: Snackbar.LENGTH_SHORT,
         });
-      }, 200)
-    }, 400)
+      }, 200);
+    }, 400);
+  }
+
+  renderSpinner() {
+    const { loading, disableAnimation } = this.props;
+    if (loading && !disableAnimation) {
+      return (
+        <Spinner type="Arc" size={250} color="#3ee8ff" isVisible />
+      );
+    } return null;
   }
 
   render() {
-    const { width, disableAnimation } = this.props;
+    const { width, disableAnimation, disabled, onPress } = this.props;
     const saveButtonContainer = {
       flex: 1,
-      flexDirection: "row",
+      flexDirection: 'row',
       width: width ? width : 200,
       height: 50,
       marginTop: 20,
-      alignSelf: "center",
-      position: "relative",
+      alignSelf: 'center',
+      position: 'relative',
       zIndex: 5
-    }
+    };
     if (disableAnimation) {
       return (
         <Animatable.View animation="fadeInUp" style={saveButtonContainer} >
-          <TouchableOpacity disabled={this.props.disabled} activeOpacity={0.6} onPress={this.props.onPress}>
+          <TouchableOpacity
+            disabled={disabled}
+            activeOpacity={0.6}
+            onPress={onPress}
+          >
             <LinearGradient
-              start={{x: 0.1, y: 0.25}}
-              end={{x: 1.6, y: 1.5}}
+              start={{ x: 0.1, y: 0.25 }}
+              end={{ x: 1.6, y: 1.5 }}
               colors={['#7e41ff', '#5AFFFB']}
               style={styles.saveButton}
-              >
+            >
               <View ref="spinner" style={styles.spinner}>
                 {this.renderSpinner()}
               </View>
 
-              <View ref="buttonTextContainer" style={{ position: "relative", bottom: 0 }}>
+              <View ref="buttonTextContainer" style={{ position: 'relative', bottom: 0 }}>
                 <Animatable.Text ref="buttonText" style={styles.buttonText}>{this.props.text}</Animatable.Text>
               </View>
 
@@ -144,29 +152,31 @@ class SaveButton extends Component {
           </TouchableOpacity>
         </Animatable.View>
       );
-    } else {
-      return (
-        <Animatable.View ref="saveButton" animation="fadeInUp" style={saveButtonContainer} >
-          <TouchableOpacity disabled={this.props.disabled} activeOpacity={0.6} onPress={this.props.onPress}>
-            <LinearGradient
-              start={{x: 0.1, y: 0.25}}
-              end={{x: 1.6, y: 1.5}}
-              colors={['#7e41ff', '#5AFFFB']}
-              style={styles.saveButton}
-              >
-              <Animatable.View ref="spinner" style={styles.spinner}>
-                {this.renderSpinner()}
-              </Animatable.View>
+    } return (
+      <Animatable.View ref="saveButton" animation="fadeInUp" style={saveButtonContainer} >
+        <TouchableOpacity
+          disabled={disabled}
+          activeOpacity={0.6}
+          onPress={onPress}
+        >
+          <LinearGradient
+            start={{ x: 0.1, y: 0.25 }}
+            end={{ x: 1.6, y: 1.5 }}
+            colors={['#7e41ff', '#5AFFFB']}
+            style={styles.saveButton}
+          >
+            <Animatable.View ref="spinner" style={styles.spinner}>
+              {this.renderSpinner()}
+            </Animatable.View>
 
-              <Animatable.View ref="buttonTextContainer" style={{ position: "relative", bottom: 0 }}>
-                <Animatable.Text ref="buttonText" style={styles.buttonText}>{this.props.text}</Animatable.Text>
-              </Animatable.View>
+            <Animatable.View ref="buttonTextContainer" style={{ position: 'relative', bottom: 0 }}>
+              <Animatable.Text ref="buttonText" style={styles.buttonText}>{this.props.text}</Animatable.Text>
+            </Animatable.View>
 
-            </LinearGradient>
-          </TouchableOpacity>
-        </Animatable.View>
-      );
-    }
+          </LinearGradient>
+        </TouchableOpacity>
+      </Animatable.View>
+    );
   }
 }
 
