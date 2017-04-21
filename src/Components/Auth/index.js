0478bc35c54ee6f5411e2575541ef07e0428f679
Ted Werbel <werbel.ted@gmail.com>
2017-04-21 10:18:00 -0400

    cleaned up auth component in prep for new design

--- src/Components/Auth/index.js
+++ src/Components/Auth/index.js
@@ -1,75 +1,77 @@
 import React, { Component } from 'react';
+import { View, StatusBar, Text, ScrollView } from 'react-native';
 import { connect } from 'react-redux';
-// Redux Actions
 import { loginUser, emailChanged, passwordChanged, registerUser, resetAuth, hideRegister } from '../../Actions';
-// Router Actions
-import { Actions } from 'react-native-router-flux';
-// UI Components
-import { StatusBar } from 'react-native';
-// import Modal from 'react-native-modalbox';
-import { View, Card, Button, Spinner, TextInput, Text, Title, Subtitle, Caption } from '@shoutem/ui';
-// Custom Components
-import { CardSection } from '../Common';
-import RegisterForm from './registerForm';
-import LoginForm from './loginForm';
-import AuthMenu from './authMenu';
-// Custom Stylesheet
-import { styles } from './authStyles';
+import { GradientView } from '../Common';
+
+/*
+1. New Account Button
+2. Login Button
+
+Components:
+1. Auth
+- Renders either <Register />
+
+*/
 
 class Auth extends Component {
   constructor(props) {
     super(props);
+    this.handleLogin = this.handleLogin.bind(this);
 
     this.state = ({
       modalOpen: false,
-      authPage: "signIn"
+      authPage: 'signIn'
     });
   }
-  onButtonPress() {
+
+  handleLogin() {
     const { email, password } = this.props;
     this.props.loginUser({ email, password });
   }
+
+  /*
   renderButtons() {
     if (this.props.loading) {
       return <Spinner size="large" />;
     }
     return (
-      <View style={styles.buttonBar}>
-        <View style={{ flex: 1, flexGrow: 1 }}>
-          <Button styleName="dark" style={styles.loginButton} onPress={this.onButtonPress.bind(this)}>
-            <Text>Sign in</Text>
-          </Button>
-          <Button styleName="clear" style={styles.registerButton} onPress={() => this.setState({ modalOpen: true })}>
-            <Caption style={styles.registerText}>Dont have an account? Sign up</Caption>
-          </Button>
-        </View>
+      <View>
+        <Text>Test</Text>
       </View>
     );
   }
+  */
+
+  /*
   renderRouter() {
-    if (this.props.currentPage === "signIn") {
-      return(
+    if (this.props.currentPage === 'signIn') {
+      return (
         <LoginForm />
       );
-    } else if (this.props.currentPage === "signUp") {
-      return(
+    } else if (this.props.currentPage === 'signUp') {
+      return (
         <RegisterForm />
       );
     }
   }
+  */
+
   render() {
-    return(
-      <View style={styles.parentContainer}>
-        <StatusBar
-         hidden
-       />
-       {this.renderRouter()}
+    return (
+      <View style={{ flex: 1 }}>
+        <GradientView style={{ height: 200 }} colors={['#AC70FB', '#9197F8', '#77BDF5']}>
+          <StatusBar hidden />
+          <ScrollView>
+            <Text style={{ backgroundColor: 'transparent' }}>Test</Text>
+          </ScrollView>
+        </GradientView>
       </View>
     );
   }
 }
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return { email, password, error, loading, showRegisterModal, currentPage } = state.auth;
 };
 
