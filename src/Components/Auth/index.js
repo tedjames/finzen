eb45a1d359d4af55d6a86fa7b4af332c905b3e1f
Ted Werbel <werbel.ted@gmail.com>
2017-04-21 21:36:47 -0400

    updated login screen design

--- src/Components/Auth/index.js
+++ src/Components/Auth/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { View, StatusBar, Text, TextInput, Image, TouchableOpacity, Dimensions } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import { connect } from 'react-redux';
 import { loginUser, emailChanged, passwordChanged, registerUser, resetAuth, hideRegister } from '../../Actions';
 import { GradientView } from '../Common';
@@ -26,9 +27,11 @@ const styles = {
     fontWeight: '600',
     letterSpacing: 17,
     position: 'absolute',
-    top: 95,
-    left: 30
-
+    top: 105,
+    left: 30,
+    textShadowColor: 'rgba(0, 0, 0, 0.25)',
+    textShadowOffset: { width: 4, height: 5 },
+    textShadowRadius: 12
   },
   zen: {
     backgroundColor: 'transparent',
@@ -39,31 +42,32 @@ const styles = {
     letterSpacing: 17,
     zIndex: 10,
     position: 'absolute',
-    top: 162,
-    left: 75
+    top: 165,
+    left: 75,
+    textShadowColor: 'rgba(0, 0, 0, 0.2)',
+    textShadowOffset: { width: 4, height: 5 },
+    textShadowRadius: 12
   },
   authBackground: {
     flex: 1,
-    resizeMode: 'cover',
     width: '100%',
     alignSelf: 'center',
-    marginTop: -220,
-    zIndex: -1
   },
   loginContainer: {
     backgroundColor: 'white',
-    height: 185,
-    width: '80%',
+    height: 195,
+    width: 300,
     marginRight: 25,
     alignSelf: 'center',
-    marginTop: 400,
+    justifyContent: 'center',
+    marginTop: 175,
     padding: 30,
     paddingBottom: 35,
     paddingTop: 35
   },
   label: {
     fontFamily: 'Montserrat',
-    fontSize: 12,
+    fontSize: 11,
     letterSpacing: 1.4,
     color: '#879099',
     opacity: 0.85,
@@ -72,9 +76,16 @@ const styles = {
   },
   field: {
     height: 25,
+    fontFamily: 'Open Sans',
     fontSize: 15,
     letterSpacing: 2,
-    fontWeight: '500'
+    fontWeight: '400'
+  },
+  divider: {
+    height: 1,
+    marginTop: 15,
+    marginBottom: 20,
+    backgroundColor: '#eee'
   },
   loginButton: {
     height: 75,
@@ -90,19 +101,23 @@ const styles = {
     color: 'white'
   },
   registerText: {
+    flex: 1,
     color: 'white',
     fontFamily: 'Montserrat',
     fontWeight: '500',
     fontSize: 12,
     letterSpacing: 3.4,
     opacity: 0.55,
-    backgroundColor: 'transparent'
+    backgroundColor: 'transparent',
+    alignSelf: 'center'
   },
   registerButton: {
     flex: 1,
-    marginLeft: 25,
-    width: 200,
-    bottom: 25
+    width: 175,
+    flexDirection: 'row',
+    position: 'relative',
+    bottom: width / 8.5,
+    left: width / 8
   }
 };
 
@@ -113,7 +128,6 @@ class Auth extends Component {
 
     this.state = ({
       modalOpen: false,
-      authPage: 'signIn'
     });
   }
 
@@ -124,43 +138,53 @@ class Auth extends Component {
 
   render() {
     return (
-      <View style={{ flex: 1 }}>
+      <KeyboardAwareScrollView
+        scrollEnabled={false}
+        extraScrollHeight={25}
+        resetScrollToCoords={{ x: 0, y: 0 }}
+        contentContainerStyle={{ flex: 1 }}
+        style={{ backgroundColor: '#000' }}
+      >
         <StatusBar hidden />
-        <GradientView style={{ height: 200 }} colors={['#AC70FB', '#9197F8', '#77BDF5']} />
+        <GradientView start={{ x: 0.0, y: 0.25 }} end={{ x: 0.5, y: 1.0 }} style={{ height: 200 }} colors={['#AC70FB', '#9197F8', '#77BDF5']} />
         <Text style={styles.fin}>FIN</Text>
         <Text style={styles.zen}>ZEN</Text>
 
         <Image
-          source={require('../../Images/loginBackground.png')}
+          source={require('../../Images/authBackground.png')}
           style={styles.authBackground}
         >
-          <View style={styles.loginContainer}>
-            <View style={styles.emailField}>
-              <Text style={styles.label}>USERNAME</Text>
-              <TextInput
-                style={styles.field}
-                placeholder={'your@email.com'}
-                onChangeText={text => this.props.emailChanged(text)}
-                value={this.props.email}
-                returnKeyType={'done'}
-                autoCapitalize={'none'}
-                autoCorrect={false}
-              />
-            </View>
-            <View style={{ marginTop: 30 }}>
-              <Text style={styles.label}>PASSWORD</Text>
-              <TextInput
-                style={styles.field}
-                placeholder={'*******'}
-                onChangeText={text => this.props.passwordChanged(text)}
-                value={this.props.password}
-                autoCorrect={false}
-                returnKeyType={'done'}
-                autoCapitalize={'none'}
-                secureTextEntry
-              />
+          <View>
+            <View style={styles.loginContainer}>
+              <View style={styles.emailField}>
+                <Text style={styles.label}>USERNAME</Text>
+                <TextInput
+                  style={styles.field}
+                  placeholder={'your@email.com'}
+                  onChangeText={text => this.props.emailChanged(text)}
+                  value={this.props.email}
+                  returnKeyType={'done'}
+                  autoCapitalize={'none'}
+                  autoCorrect={false}
+                />
+              </View>
+              <View style={styles.divider} />
+              <View>
+                <Text style={styles.label}>PASSWORD</Text>
+                <TextInput
+                  style={styles.field}
+                  placeholder={'*******'}
+                  onChangeText={text => this.props.passwordChanged(text)}
+                  value={this.props.password}
+                  autoCorrect={false}
+                  returnKeyType={'done'}
+                  autoCapitalize={'none'}
+                  secureTextEntry
+                />
+              </View>
             </View>
           </View>
+
 
           <TouchableOpacity style={styles.loginButton} activeOpacity={0.6}>
             <GradientView style={{ height: '100%', justifyContent: 'center' }} colors={['#AC70FB', '#9197F8', '#77BDF5']}>
@@ -172,7 +196,7 @@ class Auth extends Component {
             <Text style={styles.registerText}>NEW ACCOUNT</Text>
           </TouchableOpacity>
         </Image>
-      </View>
+      </KeyboardAwareScrollView>
     );
   }
 }
@@ -181,4 +205,11 @@ const mapStateToProps = (state) => {
   return { email, password, error, loading, showRegisterModal, currentPage } = state.auth;
 };
 
-export default connect(mapStateToProps, { emailChanged, passwordChanged, loginUser, registerUser, resetAuth, hideRegister })(Auth);
+export default connect(mapStateToProps, {
+  emailChanged,
+  passwordChanged,
+  loginUser,
+  registerUser,
+  resetAuth,
+  hideRegister
+})(Auth);
