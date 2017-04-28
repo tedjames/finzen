6d0530402457cc164583c3732ef0964ef19aa75f
Ted Werbel <werbel.ted@gmail.com>
2017-04-27 22:25:21 -0400

    updated Divider component for transaction listview

--- src/Components/Main/Transactions/Divider.js
+++ src/Components/Main/Transactions/Divider.js
@@ -2,41 +2,44 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 const styles = {
-  dayDivider: {
-    backgroundColor: 'transparent',
-    borderBottomWidth: 0,
-    borderColor: '#eaeff1',
-    marginTop: 10
+  container: {
+    marginTop: 20,
+    marginLeft: 5,
+    marginRight: 5,
+    marginBottom: 10,
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    alignSelf: 'center',
+    justifyContent: 'space-between'
   },
   date: {
     fontFamily: 'Open Sans',
     fontSize: 10,
     letterSpacing: 2.75,
     backgroundColor: 'transparent',
-    color: '#999cac', // #999cac is a nice color for stuff that's usually white! (blue/white) OR #a8aaba
+    color: '#878a96', // #999cac is a nice color for stuff that's usually white! (blue/white) OR #a8aaba
     fontWeight: '600',
-    alignSelf: 'center',
-    marginTop: 10
+    marginBottom: 5,
+    alignSelf: 'center'
   },
-  date2: {
-    fontFamily: 'Open Sans',
-    fontSize: 14,
-    letterSpacing: 1.25,
-    backgroundColor: 'transparent',
-    color: '#444', // #999cac is a nice color for stuff that's usually white! (blue/white) OR #a8aaba
-    fontWeight: '300',
-    alignSelf: 'flex-start',
-    marginLeft: 15,
-    marginTop: 10,
-    marginBottom: 10
+  divider: {
+    height: 1,
+    backgroundColor: '#e3e3e3',
+    flex: 1,
+    bottom: 2,
+    marginRight: 15,
+    marginLeft: 15
   }
 };
 
 export default class Divider extends Component {
   render() {
     return (
-      <View style={styles.dayDivider}>
-        <Text style={styles.date2}>{this.props.date}</Text>
+      <View style={styles.container}>
+        <View style={styles.divider} />
+        <Text style={styles.date}>{this.props.date.toUpperCase()}</Text>
+        <View style={styles.divider} />
       </View>
     );
   }
