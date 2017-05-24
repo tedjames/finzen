6698f10f191627cf8e2dbc3adc5432e311933bc7
Ted Werbel <werbel.ted@gmail.com>
2017-05-24 13:30:19 -0400

    changed accounts header text styling

--- src/Components/Main/Overview/Accounts.js
+++ src/Components/Main/Overview/Accounts.js
@@ -1,5 +1,5 @@
 import React, { PropTypes } from 'react'
-import { View } from 'react-native'
+import { View, Text } from 'react-native'
 import AccountCard from './AccountCard'
 import { Header } from '../../Common'
 
@@ -10,25 +10,36 @@ const styles = {
     shadowOpacity: 0.1,
     shadowRadius: 2,
     elevation: 1,
-    backgroundColor: "#fff",
+    backgroundColor: '#fff',
     marginRight: 0,
     marginLeft: 15,
     borderBottomLeftRadius: 2,
     borderTopLeftRadius: 2,
     borderTopRightRadius: 2,
     borderBottomRightRadius: 2
-  }
+  },
+  text: {
+    fontFamily: 'Open Sans',
+    fontSize: 20,
+    letterSpacing: 0.3,
+    backgroundColor: 'transparent',
+    color: '#333', // #999cac is a nice color for stuff that's usually white! (blue/white) OR #a8aaba
+    fontWeight: '300',
+    marginLeft: 30,
+    marginTop: 22,
+    marginBottom: 12
+  },
 };
 
 const Accounts = (props) => {
   return (
     <View>
-      <Header title="ACCOUNTS" type="section" type="leftSection" />
+      <Text style={styles.text}>Accounts</Text>
       <View style={styles.accountsCard}>
-        <AccountCard type="checking" amount="5127" name="Chase Checking"/>
-        <AccountCard type="savings" amount="271382" name="Well Fargo Savings"/>
-        <AccountCard type="cash" amount="64" name="My Wallet"/>
-        <AccountCard type="bitcoin" amount="2.748271" name="Coinbase"/>
+        <AccountCard type="checking" amount="5127" name="Chase Checking" />
+        <AccountCard type="savings" amount="271382" name="Well Fargo Savings" />
+        <AccountCard type="cash" amount="64" name="My Wallet" />
+        <AccountCard type="bitcoin" amount="2.748271" name="Coinbase" />
       </View>
     </View>
 
