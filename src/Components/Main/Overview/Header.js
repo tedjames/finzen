0cf7764d5acfaeae6cfdd66f20d122188c5b1395
Ted Werbel <werbel.ted@gmail.com>
2017-05-24 10:36:51 -0400

    removed header text and animations

--- src/Components/Main/Overview/Header.js
+++ src/Components/Main/Overview/Header.js
@@ -1,46 +1,21 @@
 import React, { Component } from 'react';
-import { Text, View, TouchableOpacity, ActionSheetIOS } from 'react-native';
+import { View, TouchableOpacity, ActionSheetIOS } from 'react-native';
 import Icon from 'react-native-vector-icons/Octicons';
 import Icon2 from 'react-native-vector-icons/FontAwesome';
 import { Actions } from 'react-native-router-flux';
 
 const styles = {
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
   icon: {
     backgroundColor: 'transparent',
     alignSelf: 'center'
   },
   iconContainer: {
-    paddingRight: 20,
-    paddingLeft: 20,
-    paddingTop: 20,
-    paddingBottom: 20
-  },
-  text: {
-    fontFamily: 'Montserrat',
-    fontSize: 12,
-    fontWeight: '400',
-    color: '#444',
-    letterSpacing: 4,
-    backgroundColor: 'transparent',
-    textShadowColor: 'rgba(0, 0, 0, 0.025)',
-    textShadowOffset: { width: 1, height: 1 },
-    textShadowRadius: 1
-  },
-  headerContainer: {
-    paddingTop: 10,
-    zIndex: 10,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    paddingRight: 5,
-    paddingLeft: 5,
-    shadowColor: '#000',
-    shadowOffset: { width: 3, height: 4 },
-    shadowOpacity: 0.125,
-    shadowRadius: 8,
-    borderBottomRightRadius: 0,
-    borderBottomLeftRadius: 0,
-    backgroundColor: '#fff'
+    padding: 20
   },
 };
 
@@ -61,13 +36,13 @@ export default class Header extends Component {
 
   render() {
     return (
-      <View style={styles.headerContainer}>
-        <TouchableOpacity style={styles.iconContainer} onPress={this.handleAdd}>
-          <Icon name="plus" size={14} style={styles.icon} color="#777" />
+      <View style={styles.header}>
+        <TouchableOpacity onPress={this.handleAdd} style={styles.iconContainer}>
+          <Icon name="plus" size={16} style={styles.icon} color="#777" />
         </TouchableOpacity>
-        <Text style={styles.text}>FINZEN</Text>
-        <TouchableOpacity style={styles.iconContainer} onPress={this.handleSettings}>
-          <Icon2 name="cogs" size={14} style={styles.icon} color="#777" />
+
+        <TouchableOpacity onPress={() => Actions.settings()} style={styles.iconContainer}>
+          <Icon2 name="cogs" size={16} style={styles.icon} color="#777" />
         </TouchableOpacity>
       </View>
     );
