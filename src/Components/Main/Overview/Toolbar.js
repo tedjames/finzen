f9caadf1655ab4c5c5fcb153b153a0c1899dec16
Ted Werbel <werbel.ted@gmail.com>
2017-05-30 11:07:01 -0400

    finished parralax effect animation for the toolbar

--- src/Components/Main/Overview/Toolbar.js
+++ src/Components/Main/Overview/Toolbar.js
@@ -1,22 +1,28 @@
 import React, { Component } from 'react';
-import { View, ScrollView } from 'react-native';
+import { View, ScrollView, Animated } from 'react-native';
 import Tool from './Tool';
 
 export default class Toolbar extends Component {
   render() {
+    const toolbarPosition = this.props.scrollY.interpolate({
+      inputRange: [0, 175],
+      outputRange: [0, 15]
+    });
     return (
-      <ScrollView
-        style={{ height: 100, marginTop: 35, marginBottom: -10 }}
-        showsHorizontalScrollIndicator={false}
-        horizontal
-      >
-        <Tool name="Budgets" white />
-        <Tool name="Bills" badgeCount={1} />
-        <Tool name="Income" white />
-        <Tool name="Savings" />
-        <Tool name="Net Worth" white badgeCount={21} />
-        <View style={{ width: 12 }} />
-      </ScrollView>
+      <Animated.View style={{ top: toolbarPosition }}>
+        <ScrollView
+          style={{ height: 120, marginTop: 25, paddingTop: 20, marginBottom: -10 }}
+          showsHorizontalScrollIndicator={false}
+          horizontal
+        >
+          <Tool name="Budgets" white />
+          <Tool name="Bills" badgeCount={1} />
+          <Tool name="Income" white />
+          <Tool name="Savings" />
+          <Tool name="Net Worth" white badgeCount={21} />
+          <View style={{ width: 12 }} />
+        </ScrollView>
+      </Animated.View>
     );
   }
 }
